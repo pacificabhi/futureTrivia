77149bca1df73eef0fd93102dd00ee10994a669d
abhishekad7 <abhishekad7@gmail.com>
2019-03-27 18:27:52 +0530

    bug fixed

--- futuretrivia/apps/users/static/users/users.js
+++ futuretrivia/apps/users/static/users/users.js
@@ -119,3 +119,33 @@ function do_request(user){
 	xhttp.send(q);
 }
 
+
+
+function show_popup(text){
+	document.getElementById("alert_data").innerHTML=text;
+	var ele = document.getElementById("alert_popup_container");
+	ele.style.display="flex";
+	//ele.style.zIndex="+999999";
+	ele.style.opacity="1";
+
+	setTimeout(function(){
+		ele.style.opacity="0";
+	}, 1800);
+
+	setTimeout(function(){
+		ele.style.display="none";
+	}, 2000);
+}
+
+function show_top_popup(text, close, closetime){
+	document.getElementById("top_popup_text").innerHTML=text;
+	var ele = document.getElementById("top_popup");
+	ele.style.top="0px";
+	
+	if(close){
+		setTimeout(function(){
+			ele.style.top="-300px";
+		}, closetime);
+	}	
+
+}
